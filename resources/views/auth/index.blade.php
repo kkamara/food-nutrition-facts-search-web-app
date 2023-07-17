952fa8bafb64ca646675e835b4ba6c0446ece79c
kkamara <kamaracomputers@gmail.com>
2023-07-17 16:33:16 +0100

    Prepare authenticate layout

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts/main')
 
 @section('content')
+  <style>
+    .login-container {
+      color: #fff;
+    }
+  </style>
+  <div class='container login-container'>
+    <div class='row'>
+      <div class='col-md-6'>
+        <h3 class='login-header'>Login</h3>
+      </div>
+      <div class='col-md-6'>
+        <h3 class='signup-header'>Signup</h3>
+      </div>
+    </div>
+  </div>
 
 @stop
