859656a49725d1cf61df0e9a478112ed929e22e0
kkamara <kamaracomputers@gmail.com>
2023-07-17 16:40:03 +0100

    Design login view

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -5,11 +5,50 @@
     .login-container {
       color: #fff;
     }
+    .submitBtn {
+      width: 90px;
+    }
+    .submitBtnContainer {
+      margin-top: 20px;
+      text-align:right;
+    }
+    .submitForm {
+      text-align:left;
+    }
   </style>
   <div class='container login-container'>
     <div class='row'>
       <div class='col-md-6'>
         <h3 class='login-header'>Login</h3>
+
+        <form class='submitForm'>
+          <div class='form-group'>
+            <label for='email'>Email:</label>
+            <input 
+              type='text'
+              name='email'
+              class='form-control'
+              placeholder='Email Address'
+            />
+          </div>
+
+          <div class='form-group'>
+            <label for='password'>Password:</label>
+            <input 
+              type='password'
+              name='password'
+              class='form-control'
+              placeholder='Password'
+            />
+          </div>
+
+          <div class='form-group submitBtnContainer'>
+            <input 
+              type='submit'
+              class='submitBtn form-control btn btn-primary btn-sm'
+            />
+          </div>
+        </form>
       </div>
       <div class='col-md-6'>
         <h3 class='signup-header'>Signup</h3>
